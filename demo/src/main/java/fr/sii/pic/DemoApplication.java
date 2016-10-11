f570e3f6166998a4552cc337ec10b56ac87b3781
Matthieu Jaulin <mjaulin.prestataire@voyages-sncf.com>
2016-10-11 09:53:40 +0200

    Ajout d'un projet java demo

---
+++ demo/src/main/java/fr/sii/pic/DemoApplication.java
@@ -0,0 +1,15 @@
+package fr.sii.pic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matthieu_jaulin
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/fr/sii/pic/DemoController.java
@@ -0,0 +1,16 @@
+package fr.sii.pic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author matthieu_jaulin
+ */
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
